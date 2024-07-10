01c6dce28f2a6905f7dd291250f2f1f65bcb7126
JenniferC57 <00160223@uca.edu.sv>
2024-07-09 22:06:19 -0600

    Agregandoo comentarios del Login y Nuevo Usuario

--- Banco_Central_Nlogonia/src/main/java/com/example/banco_central_nlogonia/LoginController.java
+++ Banco_Central_Nlogonia/src/main/java/com/example/banco_central_nlogonia/LoginController.java
@@ -8,77 +8,90 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class LoginController {
+public class LoginController { //00160223 Inicio de la logica de la interfaz del Login
 
     @FXML
-    private Label lblBienvenida;
+    private Label lblBienvenida; //00160223 Declarando Label de "Bienvenida".
 
     @FXML
-    private Label lblUsuario;
+    private Label lblUsuario; //00160223 Declarando Label de "Usuario".
 
     @FXML
-    private TextField txtFUsuario;
+    private Label lblInformacionUsuario; //00160223 Declarando Label de "Informacion para el Usuario".
 
     @FXML
-    private Label lblContrasena;
+    private Label lblInfomacionAdministrador; //00160223 Declarando Label de "Informacion para el administrador".
 
     @FXML
-    private TextField txtFContrasena;
+    private TextField txtFUsuario; //00160223 Declarando el TextField del Label "Usuario"
 
     @FXML
-    private Button btnIngresar;
+    private Label lblContrasena; //00160223 Declarando el Label de "Contraseña"
 
     @FXML
-    private Label lblCrearUsuario;
+    private TextField txtFContrasena; //00160223 Declarando el TextField del Label "Contraseña"
 
     @FXML
-    private Button btbNuevoUsuario;
+    private Button btnIngresar; //00160223 Declarando el Button para Ingresar y seguir a la siguiente pantalla
 
     @FXML
-    private Label lblError;
-
-
-    /*
-    @FXML
-    public void initialize() {
-        lblError.setText("");
-    }*/
+    private Label lblCrearUsuario; //00160223 Declarando el Label de "Crear Usuario"
 
     @FXML
-    private void changeScene(ActionEvent event, String File) {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource(File));
-                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                Scene scene = new Scene(loader.load());
-                stage.setScene(scene);
-                stage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
+    private Button btbNuevoUsuario; //00160223 Declarando el Button de Nuevo Usuario
+
+    @FXML
+    private Label lblError; //00160223 Declarando el Label de Error por si el usuario no completa los datos solicitados
+
+    @FXML
+    private Button btnAdministrador; //00160223 Declarando el Button de Administrador
+
+    @FXML
+    private Line lineLineaDivisora; //00160223 Declarando una Line para dividir el inicio de sesion del usuario ya registrado y del nuevo usuario
+
+
+
+    @FXML
+    public void initialize() { //00160223 Inizializando la Interfaz
+        lblError.setText(""); //00160223 Limpia el area del texto del Label error.
+    }
+
+    @FXML
+    private void changeScene(ActionEvent event, String File) { //00160223 Metodo para poder cambiar de pantalla en los diferentes metodos donde se necesite este metodo.
+            try { //00160223 Encapsulamiento del codigo a ejecutar, por si ocurre una excepcion.
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(File)); //00160223 Aqui se Carga el archivo fxml
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //00160223 Obtiene el escenario, la ventana actual
+                Scene scene = new Scene(loader.load()); //00160223 Se carga la nueva escena a la que se cambio
+                stage.setScene(scene); //00160223 Configuracion del Stage con la nueva pantalla
+                stage.show(); //00160223 Muestra la nueva pantalla
+            } catch (IOException e) { //00160223 Captura la excepcion y tira un IOException
+                e.printStackTrace(); //00160223 Proporciona los datos de la excepcion
             }
     }
 
     @FXML
-    private void IngresarDatosLogin(ActionEvent event) {
+    private void IngresarDatosLogin(ActionEvent event) { //00160223 Metodo que se ejecuta cuando el usuario le da al botón de Ingresar
 
-        String usuario = txtFUsuario.getText();
-        String contrasena = txtFContrasena.getText();
+        String usuario = txtFUsuario.getText(); //00160223 Obteniendo el dato establecido para el usuario registrado
+        String contrasena = txtFContrasena.getText(); //00160223 Obteniendo la contraseña establecida del usuario registrado
 
-        if (usuario.isEmpty() || contrasena.isEmpty()) {
-            lblError.setText("Por favor ingrese un usuario y contraseña.");
-        } else {
-            lblError.setText("");
-            System.out.println("Iniciando sesion con el usuario: " + usuario);
-            changeScene(event, "Transaccion-view.fxml");
+        if (usuario.isEmpty() || contrasena.isEmpty()) { //00160223 Condicion if, que verifica si el textField del usuario o contraseña estan vacios
+            lblError.setText("Por favor ingrese un usuario y contraseña."); //00160223 Si la condicion se cumple, enseña un texto en el Label de Error.
+        } else { //00160223 Si la condicion no se cumple, pasa a lo siguiente:
+            lblError.setText(""); //00160223 Muestra el Label de Error vacio
+            System.out.println("Iniciando sesion con el usuario: " + usuario); //00160223 Mostrando mensaje de Iniciando Sesion con el usuario ingresado
+            changeScene(event, "Transaccion-view.fxml"); //00160223 Estableciendo el nombre del archivo fxml con el que queremos el cambio de escena
         }
     }
 
     @FXML
-    private void IngresoNuevoUsuario(ActionEvent event) {
-        changeScene(event, "NuevoUsuario-view.fxml");
+    private void IngresoNuevoUsuario(ActionEvent event) { //00160223 Metodo que esta establecido en el botón de "Nuevo Usuario"
+        changeScene(event, "NuevoUsuario-view.fxml"); //00160223 Estableciendo el nombre del archivo fxml con el que queremos el cambio de escena
     }
 
 }
